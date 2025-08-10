3951f9c3318818748819aef83920859850718a0c
JoaoKraideM <joao.kraide@gmail.com>
2025-08-10 19:42:19 -0300

    configurando main pra subir

--- main.py
+++ main.py
@@ -22,7 +22,3 @@ app.css_folder = 'static/css'
 # Importa as rotas do arquivo views.py
 from views import *
 
-# Inicia o servidor
-if __name__ == '__main__':
-    socketio = SocketIO(app)
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
